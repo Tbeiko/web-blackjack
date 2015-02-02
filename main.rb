bbdd8020e141b0c3bb0e6158b24a812352666454
Tim Beiko <t.beiko23@gmail.com>
2015-02-02 09:46:48 +0800

    Do Prep Work on game

--- main.rb
+++ main.rb
@@ -1,8 +1,16 @@
 require 'rubygems'
 require 'sinatra'
+require "sinatra/reloader" if development?
 
-set :sessions, true
+use Rack::Session::Cookie, :key => 'rack.session',
+                           :path => '/',
+                           :secret => 'live_life_abroad' 
 
 
+get "/name" do
+  "my name is Timothe Beiko"
+end
 
-
+get "/template" do
+  erb :profile
+end
